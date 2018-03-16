ead732e5b3931ff52ede0e0a49cfbd466476d213
Dryksd <samara.lawfirm@gmail.com>
2018-03-16 23:26:43 +0300

    pip 8

--- 1.py
+++ 1.py
@@ -1,40 +1,52 @@
-class Non_human():
+class NonHuman:
     brain = 2
     tail = 1
     head = 1
 
-class Animals(Non_human):
-    def __init__(self):
-        self.legs = 4
+
+class Animals(NonHuman):
+    legs = 4
 
     def power(self, meal):
+        self.meal = meal
         if meal > 0:
             self.strong = meal*2
             return self.strong
 
-class Bird (Non_human):
+
+class Bird(NonHuman):
     def __init__(self):
         self.legs = 2
 
-    def power (self, meal):
+    def power(self, meal):
         if meal > 0:
             self.strong = meal*1.5
             return self.strong
 
-class Cows(Non_human,Animals):
+
+class Cows(Animals):
     mass = 10
-class Goats(Non_human,Animals):
+
+
+class Goats(Animals):
     mass = 5
-class Sheep(Non_human,Animals):
+
+
+class Sheep(Animals):
     mass = 5
-class Pigs(Non_human,Animals):
+
+
+class Pigs(Animals):
     mass = 6
 
-class Chickens(Non_human,Bird):
+
+class Chickens(Bird):
     mass = 2
-class Gooses(Non_human,Bird):
+
+
+class Gooses(Bird):
     mass = 2
-class Ducks(Non_human,Bird):
+
+
+class Ducks(Bird):
     mass = 1
-
-
